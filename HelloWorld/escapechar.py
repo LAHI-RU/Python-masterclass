11b87eb15ce9b2f2ac776e991b8d6c6825ffb897
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-01-01 10:26:35 +0530

    Create escapechar.py

---
+++ HelloWorld/escapechar.py
@@ -0,0 +1,27 @@
+splitstring = "This string has been \nsplit over\nseveral\nlines"
+print(splitstring)
+
+tabbedString = "1\t2\t3\t2\t4\t5"
+print(tabbedString)
+
+print('The pet owner said "No, no, \'e\'s uh,...he\'s resting". ')
+# or
+print("The pet owner said \"No, no, 'e's uh,...he's resting'. ")
+
+print("""The pet owner said "No, no, 'e's uh,...he's resting'. """)
+
+# anotherSplitString = """This string has been
+# split over
+# several
+# levels
+# """
+
+# ==============
+
+anotherSplitString = """This string has been \
+split over \
+several \
+levels \
+"""
+
+print(anotherSplitString)

2ec759f55a80a838ff5c889e7317c974d3b18eef
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-02-09 09:28:54 +0530

    update strings2.py

--- HelloWorld/strings2.py
+++ HelloWorld/strings2.py
@@ -1,16 +1,47 @@
 #         012345678901234
 parrot = "Norwegain Blue"
 
-print(parrot)
+# print(parrot)
+#
+# print(parrot[13])
+# print()
+#
+#
+# # Mini challenge
+# print(parrot[3])
+# print(parrot[4])
+# print(parrot[9])
+# print(parrot[3])
+# print(parrot[7])
+# print(parrot[8])
+#
+# # negative indexes
+# print()
+# print(parrot[-11])
+# print(parrot[-10])
+# print(parrot[-5])
+# print(parrot[-11])
+# print(parrot[-7])
+# print(parrot[-6])
 
-print(parrot[13])
-print()
+# print()
+# print(parrot[3 -14])
+# print(parrot[4 -14])
+# print(parrot[9 -14])
+# print(parrot[3 -14])
+# print(parrot[7 -14])
+# print(parrot[8 -14])
 
+print(parrot[0:6])   # starts with index position 0 and extends up to 6 (but not including 6th position)
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])  # start value is 0(because if defaults to beginning
 
-# Mini challenge
-print(parrot[3])
-print(parrot[4])
-print(parrot[9])
-print(parrot[3])
-print(parrot[7])
-print(parrot[8])
+print(parrot[10:])
+
+print(parrot[:6])
+print(parrot[6:])
+
+print(parrot[:6] + parrot[6:])
+
+print(parrot[:])

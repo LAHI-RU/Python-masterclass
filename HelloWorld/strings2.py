0e9e9bb524a11da95825e889429cf0591649490e
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-02-09 07:46:51 +0530

    update strings2.py

---
+++ HelloWorld/strings2.py
@@ -0,0 +1,16 @@
+#         012345678901234
+parrot = "Norwegain Blue"
+
+print(parrot)
+
+print(parrot[13])
+print()
+
+
+# Mini challenge
+print(parrot[3])
+print(parrot[4])
+print(parrot[9])
+print(parrot[3])
+print(parrot[7])
+print(parrot[8])
